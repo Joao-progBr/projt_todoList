5c1e0aefafa52e9a53eed13dacb71cf46a989bf9
joãoA <joaovictor.dudu03@gmail.com>
2025-03-29 03:14:26 -0300

    fazendo o replace do less para css no arquivo final html

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ const gulp = require('gulp')
 const uglify = require('gulp-uglify')
 const less = require('gulp-less')
 const cleanCss = require('gulp-clean-css')
+const replace = require('gulp-replace')
 
 function moveHtml(){
     return gulp.src('./src/index.html')
+    .pipe(replace('./style/style.less', './style/style.css'))
     .pipe(gulp.dest('./dist/'))
 }
 
